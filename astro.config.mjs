592d7b961faa604de0082d8106afd4a4585043e4
Yogurt7v <egorov-2k@yandex.ru>
2025-05-04 10:29:46 +0300

    add: first dynamic island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-      site: "https://sprightly-medovik-4d9ecb.netlify.app/"
+  site: 'https://sprightly-medovik-4d9ecb.netlify.app/',
+  integrations: [preact()],
 });
